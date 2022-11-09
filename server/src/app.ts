8ef89980996c8294faa2f48e6ff94112b7b9e641
santiago <santiago.borquez.1705@gmail.com>
2022-11-09 16:54:41 -0300

    Fix server deploy

--- server/src/app.ts
+++ server/src/app.ts
@@ -20,7 +20,7 @@ interface ErrorStatus extends ErrorRequestHandler {
 dotenv.config()
 server.set('trust proxy', 1)
 server.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.ORIGIN || 'http://localhost:5173',
   methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD'],
   credentials: true
 }))
@@ -58,7 +58,7 @@ server.use(
 // server.use(cookieParser())
 server.use(morgan('dev'))
 server.use((_req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173') // 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', process.env.ORIGIN || 'http://localhost:5173') // 'http://localhost:3000'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE, PATCH')
