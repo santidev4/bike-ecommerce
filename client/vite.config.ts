9d5510097205aaad4cb9f5e93c18a9cbb4cab917
santiago <santiago.borquez.1705@gmail.com>
2022-11-12 00:45:47 -0300

    Fix env variables on vite.config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,19 +1,41 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      "/server": {
-        target: "http://localhost:3001",
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-        rewrite: (path) => path.replace(/^\/server/, ""),
+// export default defineConfig({
+//   server: {
+//     proxy: {
+//       '/server': {
+//         target: 'http://localhost:3001',
+//         changeOrigin: true,
+//         secure: false,
+//         ws: true,
+//         rewrite: (path) => path.replace(/^\/server/, '')
+//       }
+//     },
+//     port: 5173
+//   },
+//   plugins: [react()]
+// })
+
+export default ({ mode }) => {
+  // Load app-level env vars to node-level env vars.
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+  return defineConfig({
+    // To access env vars here use process.env.TEST_VAR
+    server: {
+      proxy: {
+        '/server': {
+          target: process.env.BACKEND_URL,
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+          rewrite: (path) => path.replace(/^\/server/, '')
+        }
       },
+      port: 5173
     },
-    port: 5173,
-  },
-  plugins: [react()],
-});
+    plugins: [react()]
+  })
+}
